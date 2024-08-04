69e3320ba4c02589d6f673320bb0d29bfb938501
juniorpinhodev <juniorpinhodev@gmail.com>
2024-08-04 01:21:07 -0300

    feat: 🎸 Rotas para Excluir e Editar Locais por ID

--- src/controllers/LocalController.js
+++ src/controllers/LocalController.js
@@ -61,6 +61,60 @@ class LocalController {
       return response.status(500).json({ mensagem: 'Houve um erro ao buscar o local' });
     }
   }
+
+  // Método para atualizar um local específico do usuário autenticado
+  async atualizar(request, response) {
+    try {
+      const { local_id } = request.params;
+      const { nome, endereco, descricao, coordenadas, google_maps_link } = request.body;
+      const usuarioId = request.usuarioId;
+
+      // Encontra o local específico e verifica se pertence ao usuário autenticado
+      const local = await ExerciseLocal.findOne({ where: { id: local_id, usuarioId } });
+
+      if (!local) {
+        return response.status(404).json({ mensagem: 'Local não encontrado ou acesso não autorizado' });
+      }
+
+      // Atualiza o local
+      local.nome = nome || local.nome;
+      local.localidade = endereco || local.localidade;
+      local.descricao = descricao || local.descricao;
+      local.coordenadas = coordenadas || local.coordenadas;
+      local.google_maps_link = google_maps_link || local.google_maps_link;
+
+      await local.save();
+
+      return response.status(200).json(local);
+    } catch (error) {
+      console.error(error);
+      return response.status(500).json({ mensagem: 'Houve um erro ao atualizar o local' });
+    }
+  }
+
+  // Método para excluir um local específico do usuário autenticado
+  async excluir(request, response) {
+    try {
+      const { local_id } = request.params;
+      const usuarioId = request.usuarioId;
+
+      // Encontra o local específico e verifica se pertence ao usuário autenticado
+      const local = await ExerciseLocal.findOne({ where: { id: local_id, usuarioId } });
+
+      if (!local) {
+        return response.status(404).json({ mensagem: 'Local não encontrado ou acesso não autorizado' });
+      }
+
+      // Exclui o local
+      await local.destroy();
+
+      return response.status(204).send();
+    } catch (error) {
+      console.error(error);
+      return response.status(500).json({ mensagem: 'Houve um erro ao excluir o local' });
+    }
+  }
+
 }
 
 module.exports = new LocalController();
--- src/routes/local.routes.js
+++ src/routes/local.routes.js
@@ -13,4 +13,10 @@ localRoutes.get('/', verifyToken, LocalController.listar);
 // Rota para listar um local específico do usuário autenticado
 localRoutes.get('/:local_id', verifyToken, LocalController.listarPorId);
 
+// Rota para atualizar um local específico do usuário autenticado
+localRoutes.put('/:local_id', verifyToken, LocalController.atualizar);
+
+// Rota para excluir um local específico do usuário autenticado
+localRoutes.delete('/:local_id', verifyToken, LocalController.excluir);
+
 module.exports = localRoutes;
